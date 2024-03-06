c19d6308f53c58efd7451923f6174f29f105f558
Diegocarque12 <diegocarque@hotmail.com>
2024-03-05 20:59:27 -0600

    markup the index and add the colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      white: "#fff",
+      primary_nutmeg: "#854632",
+      primary_darkRaspberry: "#7b284f",
+      neutral_roseWhite: "#fff5fa",
+      neutral_eggshell: "#f3e6d8",
+      neutral_lightGrey: "#e4ded8",
+      neutral_wengeBrown: "#5f574e",
+      neutral_darkCharcoal: "#302d2c",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
